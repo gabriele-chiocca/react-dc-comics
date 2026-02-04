18364636b353af803d650974a8a216debf62796a
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-05 00:39:52 +0100

    banner img

--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -4,19 +4,33 @@ export default function PreFooter() {
       <div className="d-flex justify-content-evenly align-items-center bg-primary py-5 prefooter">
         <div className="d-flex">
           <img src="public\img\buy-comics-digital-comics.png" alt="Logo" />
-          <p className="text-light align-self-center ms-4">DIGITAL COMICS</p>
+          <span className="text-light align-self-center ms-4 ">
+            DIGITAL COMICS
+          </span>
         </div>
-        <div>
-          <h2>p</h2>
+        <div className="d-flex">
+          <img src="public\img\buy-comics-digital-comics.png" alt="Logo" />
+          <span className="text-light align-self-center ms-4 ">
+            DIGITAL COMICS
+          </span>
         </div>
-        <div>
-          <h2>p</h2>
+        <div className="d-flex">
+          <img src="public\img\buy-comics-digital-comics.png" alt="Logo" />
+          <span className="text-light align-self-center ms-4 ">
+            DIGITAL COMICS
+          </span>
         </div>
-        <div>
-          <h2>p</h2>
+        <div className="d-flex">
+          <img src="public\img\buy-comics-digital-comics.png" alt="Logo" />
+          <span className="text-light align-self-center ms-4 ">
+            DIGITAL COMICS
+          </span>
         </div>
-        <div>
-          <h2>p</h2>
+        <div className="d-flex">
+          <img src="public\img\buy-comics-digital-comics.png" alt="Logo" />
+          <span className="text-light align-self-center ms-4 ">
+            DIGITAL COMICS
+          </span>
         </div>
       </div>
     </>
