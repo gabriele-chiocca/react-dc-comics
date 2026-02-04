b72d9416c3a2030a91e1bd0ed014df08bf993b37
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-04 14:42:16 +0100

    Structure with Header, Main, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/footer';
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   );
 }
