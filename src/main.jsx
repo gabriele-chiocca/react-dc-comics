3d6930e1819a298a4852c7cbcde51773e97655ce
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-04 14:52:48 +0100

    installed bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+// Import our custom CSS
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
