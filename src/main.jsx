8cff5b56e7cb7cff0b90a9fd10b3f1d3bcd2eceb
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-04 14:57:02 +0100

    inserted img and css order in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 
 // Import our custom CSS
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
+
+// Import Custom CSS
+
+import './assets/css/index.css';
+
+// Import root Component
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
