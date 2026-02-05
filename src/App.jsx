886deb36e370c2e00130e95d55c9e99747955dc9
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-05 16:09:02 +0100

    comics array

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import PreFooter from './components/PreFooter';
 export default function App() {
   return (
