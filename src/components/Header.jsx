c3ecb6fb7ad39dcd37af91340a3524166b2b6966
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-05 17:24:05 +0100

    navbar with .map and key

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,56 @@
 export default function Header() {
+  const navElements = [
+    {
+      url: '/',
+      text: 'CHARACTERS',
+    },
+
+    {
+      url: '/comics',
+      text: 'COMICS',
+    },
+
+    {
+      url: '/movies',
+      text: 'MOVIES',
+    },
+
+    {
+      url: '/tv',
+      text: 'TV',
+    },
+
+    {
+      url: '/games',
+      text: 'GAMES',
+    },
+
+    {
+      url: '/collectibles',
+      text: 'COLLECTIBLES',
+    },
+
+    {
+      url: '/videos',
+      text: 'VIDEOS',
+    },
+
+    {
+      url: '/fans',
+      text: 'FANS',
+    },
+
+    {
+      url: '/news',
+      text: 'NEWS',
+    },
+
+    {
+      url: '/shop',
+      text: 'SHOP',
+    },
+  ];
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 align">
@@ -17,6 +69,16 @@ export default function Header() {
 
         <div className="me-4 collapse navbar-collapse no-grow" id="navbarNav">
           <ul className="navbar-nav nav-underlined">
+            {navElements.map((element, index) => {
+              return (
+                <li key={index} className="nav-item">
+                  <a className="nav-link fw-medium" href={element.url}>
+                    {element.text}
+                  </a>
+                </li>
+              );
+            })}
+            {/*
             <li className="nav-item">
               <a className="nav-link fw-medium" href="#">
                 CHARACTERS
@@ -66,7 +128,7 @@ export default function Header() {
               <a className="nav-link fw-medium" href="#">
                 SHOP
               </a>
-            </li>
+            </li>*/}
           </ul>
         </div>
       </nav>
