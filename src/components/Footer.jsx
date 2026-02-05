95ce410466e49698261ebf541da9ef44beab93da
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-05 15:35:39 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,227 @@
 export default function Footer() {
   return (
     <>
-      <footer>Sono il Footer</footer>
+      <footer className="bg-footer text-light mt-auto">
+        <div className="container py-5">
+          <div className="row align-items-center g-4">
+            <div className="col-12 col-md-8">
+              <div className="row g-3">
+                <div className="col-6 col-sm-4">
+                  <h5 className="text-uppercase mb-2">DC COMICS</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Chi siamo
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Lavora con noi
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Contatti
+                      </a>
+                    </li>
+                  </ul>
+
+                  <h5 className="text-uppercase mb-2 mt-3">SHOP</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Chi siamo
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Lavora con noi
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Contatti
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="col-6 col-sm-4">
+                  <h5 className="text-uppercase">DC</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Terms of Use
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Privacy policy (New)
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Ad Choices
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Advertising
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Jobs
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Subscription
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Talent Workshops
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        CPSC Certificates
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Ratings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Shop Help
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        Contact Us
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                {/* Terzo Li */}
+                <div className="col-6 col-sm-4">
+                  <h5 className="text-uppercase">SITES</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        DC
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        MAD Magazine
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        DC Kids
+                      </a>
+                    </li>
+                  </ul>
+
+                  <h5 className="text-uppercase mb-2 mt-3">SHOP</h5>
+                  <ul className="list-unstyled mb-0">
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        DC Universe
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="link-light link-underline-opacity-0 link-underline-opacity-75-hover"
+                        href="#"
+                      >
+                        DC Power Visa
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-12 col-md-4">
+              <img
+                src="public\img\dc-logo-bg.png"
+                alt="Immagine footer"
+                className="img-fluid rounded shadow-sm"
+                style={{ maxWidth: '380px' }}
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
