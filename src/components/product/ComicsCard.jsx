7893b9d83696cb5a817c0bcfcc5f29e8be5de927
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-06 16:53:23 +0100

    creating card component

--- src/components/product/ComicsCard.jsx
+++ src/components/product/ComicsCard.jsx
@@ -1,7 +1,4 @@
-import Jumbotron from './jumbotron';
-import ComicsCard from './product/ComicsCard.jsx';
-
-export default function Main() {
+export default function ComicsCard() {
   const comics = [
     {
       id: 1,
@@ -180,9 +177,36 @@ export default function Main() {
   ];
 
   return (
-    <>
-      <Jumbotron></Jumbotron>
-      <ComicsCard></ComicsCard>
-    </>
+    <div className="bg-dark py-4">
+      <div className="container relative">
+        <span className="bg-primary text-light p-3 h5 absolute">
+          CURRENT SERIES
+        </span>
+
+        <div className="row">
+          {comics.map((comic) => {
+            return (
+              <div key={comic.id} className="col-lg-2 col-md-4 col-6">
+                <div className="my-4">
+                  <div className="img-wrapper">
+                    <img
+                      className="img-fluid comic-img"
+                      src={comic.thumb}
+                      alt={comic.title}
+                    />
+                  </div>
+                  <p className="text-light h5 align-title-comics">
+                    {comic.title}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="text-center pb-4">
+        <button className="btn btn-primary">LOAD MORE</button>
+      </div>
+    </div>
   );
 }
