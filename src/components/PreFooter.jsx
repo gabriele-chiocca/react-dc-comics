88785359caf46825a2ec929643030ae9ddaf0023
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-06 17:11:37 +0100

    adjusted responsive for blue banner

--- src/components/PreFooter.jsx
+++ src/components/PreFooter.jsx
@@ -1,32 +1,32 @@
 export default function PreFooter() {
   return (
     <>
-      <div className="d-flex justify-content-evenly align-items-center bg-primary py-5 prefooter">
+      <div className="d-flex flex-container justify-content-evenly align-items-center bg-primary py-5 prefooter">
         <div className="d-flex">
           <img src="public\img\buy-comics-digital-comics.png" alt="Logo" />
           <span className="text-light align-self-center ms-4 ">
             DIGITAL COMICS
           </span>
         </div>
-        <div className="d-flex">
+        <div className="d-flex flex-item">
           <img src="public\img\buy-comics-merchandise.png" alt="Logo" />
           <span className="text-light align-self-center ms-4 ">
             DC MERCHANDISE
           </span>
         </div>
-        <div className="d-flex">
+        <div className="d-flex flex-item">
           <img src="public\img\buy-comics-subscriptions.png" alt="Logo" />
           <span className="text-light align-self-center ms-4 ">
             SUBSCRIPTION
           </span>
         </div>
-        <div className="d-flex">
+        <div className="d-flex flex-item">
           <img src="public\img\buy-comics-shop-locator.png" alt="Logo" />
           <span className="text-light align-self-center ms-4 ">
             COMIC SHOP LOCATOR
           </span>
         </div>
-        <div className="d-flex">
+        <div className="d-flex flex-item">
           <img src="public\img\buy-dc-power-visa.svg" alt="Logo" />
           <span className="text-light align-self-center ms-4 ">
             DC POWER VISA
